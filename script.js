13bbec8443526b3bce5bbb8d22b9ca260c3776fc
Mohini5395 <115691287+Mohini5395@users.noreply.github.com>
2023-03-10 17:49:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const btnEl = document.getElementById('btn')
+const ratingEls = document.querySelectorAll(".rating");
+const containerEl = document.getElementById("container");
+let selectedRating = "";
+
+ratingEls.forEach((ratingEl) => {
+    ratingEl.addEventListener("click", (event) => {
+        console.log(event.target.innerText || event.target.parentNode.innerText);
+        removeActive()
+        selectedRating = event.target.innerText || event.target.parentNode.innerText;
+        event.target.classList.add("active")
+        event.target.parentNode.classList.add("active")
+    });
+});
+
+btnEl.addEventListener("click", () => {
+    if (selectedRating !== "") {
+        containerEl.innerHTML = `
+        <strong>Thank You!</strong>
+        <br>
+        <br>
+        <strong>Feedback : ${selectedRating}</strong>
+        <p>We'll use your Feedback to improve our customer services</p>
+
+        `
+    }
+})
+function removeActive() {
+    ratingEls.forEach((ratingEl) => {
+        ratingEl.classList.remove("active")
+    })
+}
